109125f901b75b3536a3854a1d9aca56551207f4
Mikolaj Narewski <dsw49525@student.dsw.edu.pl>
2023-07-01 10:13:02 +0200

    skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,14 @@ show_date() {
 create_gitignore() {
     echo "*log*" > .gitignore
 }
+
+# Funkcja tworząca pliki log
+create_logs() {
+    local num_files=${1:-100}  # Jeśli nie podano liczby plików, domyślnie tworzy 100 plików
+
+    for ((i=1; i<=$num_files; i++)); do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+        echo "Data utworzenia: $(date)" >> log$i.txt
+    done
+}
